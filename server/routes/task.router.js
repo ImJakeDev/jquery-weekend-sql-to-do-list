6308e2f99663fc593950759e5c02e39024df7c2f
Jake Schaffer <jakecr8guru@gmail.com>
2020-06-06 15:16:11 -0500

    added GET for /tasks that gets everything in DB

--- server/routes/task.router.js
+++ server/routes/task.router.js
@@ -10,4 +10,22 @@ pool.on('error', (error) => {
 	console.log('Error with postgres pool', error)
 });
 
+// GET
+
+// GETs all the tasks from database
+taskRouter.get('/', (req, res) => {
+    pool.query('SELECT * FROM "tasks" ORDER BY "task_name";')
+        .then((result) => {
+            console.log(`Here is everything in /tasks`);
+            res.status(200).send(result.rows);
+        })
+        .catch((err) => res.status(500).send(`Error Connecting to Database: ${err}`));
+});
+
+// POST
+
+// PUT
+
+// DELETE
+
 module.exports = taskRouter;
