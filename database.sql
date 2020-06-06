3ef76f1733e8a5c7f40287717109032d96dc499b
Jake Schaffer <jakecr8guru@gmail.com>
2020-06-06 11:39:52 -0500

    started setup of DB

--- database.sql
+++ database.sql
@@ -11,9 +11,17 @@ CREATE TABLE "tasks" (
 	"id" 				INT GENERATED ALWAYS AS IDENTITY, /* Always a unique identifier */
 	"task_name" 		VARCHAR(100) NOT NULL, /* A short task discription */
 	"task_details"	    TEXT, /* If needed more information is stroed here */
-	"matrix_status"		VARCHAR(11) NOT NULL, /* 4 options to choose where task is displayed in the matrix */
+	"matrix_status"		VARCHAR(11) NOT NULL, /* 4 options (Do, Schedule, Delegate, Eliminate) to choose where task is displayed in the matrix */
 	"due_date"	        DATE, /* Option for a due date */
     "schedule_date"     DATE, /* Option for a scheduling a date */
-	"urgence_level"		VARCHAR(11) NOT NULL, /* Level of urgence */
-    "importance_level"  VARCHAR(14) NOT NULL, /* Level of importance */
-    "progress_state"    VARCHAR(14) NOT NULL); /* State of progress */
+	"urgence_level"		VARCHAR(10) NOT NULL, /* Level of urgence options: Urgent or Non-urgent */
+    "importance_level"  VARCHAR(13) NOT NULL, /* Level of importance options: Important or Non-important */
+    "progress_state"    VARCHAR(14) NOT NULL); /* State of progress options: Inprogress, Complete, Eliminate, Delete */
+    /* Default is Inprogress.
+    When check box is clicked it is Complete and will "disappear" from DOM. 
+    When it is Non-urgent and Non-important it will have the state of Eliminate and is recommended to delete.
+    Delete state means it is deleted??? */
+
+/* Data example: */
+INSERT INTO "tasks" ("task_name", "task_details", "matrix_status", "due_date", "urgence_level", "importance_level", "progress_state")
+	VALUES ('Finish weekend assignment', 'This is something that needs to be done.', 'Do', 06/08/2020, 'Urgent', 'Important', 'Inprogress');
