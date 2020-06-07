18eb092b5bcd611fc9ddd6f97d62a9fd0449485f
Jake Schaffer <jakecr8guru@gmail.com>
2020-06-07 10:34:23 -0500

    started appending to DOM and POSTing to server and DB

--- server/public/scripts/client.js
+++ server/public/scripts/client.js
@@ -2,51 +2,119 @@ $(document).ready(readyNow);
 
 function readyNow() {
     console.log('Testing testing 1 2 3...');
-    $('#submitTask').click(addTaskClickHandler);
+    $('#sumbitTask').click(saveTasks);
+    refreshTasks();
 }
 
 function addTaskClickHandler() {
+    // Inputs will need conditionals with levels that will automaticly make the value for matrix status 
     const taskName = $('#taskNameIn').val();
     const taskUrgenceLevel = $('#urgenceLevelIn').val();
     const taskImportanceLevel = $('#importanceLevelIn').val();
     const taskDetails = $('#taskDetailsIn').val();
 }
 
+function saveTasks() {
+    console.log('In saveTasks function!');
+	if (validateInputs()) {
+		// checks if the inputs are infact invald, if so, return false.
+		return false;
+	}
+	// ajax call to server to get tasks
+	const taskName = $('#taskNameIn').val();
+	const taskUrgenceLevel = $('#urgenceLevelIn').val();
+	const taskImportanceLevel = $('#importanceLevelIn').val();
+	const taskDetails = $('#taskDetailsIn').val();
+
+	$.ajax({
+		method: 'POST',
+		url: '/tasks',
+		data: {
+			task_name: taskName,
+			task_details: taskDetails,
+			matrix_status: 'Do',
+            urgence_level: taskUrgenceLevel,
+            importance_level: taskImportanceLevel,
+            progress_state: 'Inprogress',
+		}
+	}).then(function (response) {
+
+		// update the history
+		refreshTasks();
+		clearInputs();
+	}).catch(function (response) {
+		// only called when server returns 4xx 5xx
+		alert('something went wrong');
+	})
+}
+
+
+function validateInputs() {
+	if ($('#taskNameIn').val() === '' ||
+		$('#urgenceLevelIn').val() === '' ||
+		$('#importanceLevelIn').val() === '' ||
+		$('#taskDetailsIn').val() === ''){
+		alert('You must fill all input fields out!')
+		return true;
+	}
+}
+
+function clearInputs() {
+	// function to clear the input fields
+	$('#taskNameIn').val('');
+	$('#urgenceLevelIn').val('');
+	$('#importanceLevelIn').val('');
+	$('#taskDetailsIn').val('');
+}
+
+// Function GETs data and displays it
 function refreshTasks() {
+    console.log('In refreshTask function!');
 	$.ajax({
 		method: 'GET',
 		url: '/tasks',
 	}).then(function (response) {
-		$('table tbody').empty();
-		for (let task of response) {
-			let rowElement = $("<tr></tr>");
-			const name = koala.name;
-			const sex = koala.sex;
-			const age = koala.age;
-			const readyToTransfer = koala.ready_to_transfer;
-			const notes = koala.notes;
-
-			rowElement.data('koala', koala);
+		$('#doTable #doTbody').empty();
+		for (const task of response) {
+            let rowElement = $('<tr class="table-success"></tr>');
+            // Will need to think more about how and where to use these
+			const name = task.task_name;
+			const details = task.task_details;
+			const matrixStatus = task.matrix_status;
+			const urgence = task.urgence_level;
+			const importance = task.importance_level;
+            const progressState = task.progress_state;
+            // What do I actually need to append in the table?
+            rowElement.data('task', task);
+            // Need to make this a variable that increments
+            rowElement.append(`<th scope="row">1</th>`);
+            //////////////////////////////////////////////
 			rowElement.append(`<td>${name}</td>`);
-			rowElement.append(`<td>${sex}</td>`);
-			rowElement.append(`<td>${age}</td>`);
-			if (readyToTransfer === true) {
+			// rowElement.append(`<td>${details}</td>`);
+            // rowElement.append(`<td>${progressState}</td>`);
+            // If matrix status is 'Do' append here:
+            // I could use this checkbox for my progress state...
+			if (matrixStatus === 'Do') {
 				let checkbox = $('<input type="checkbox"/>').prop('checked', true).prop('disabled', true);
 				let td = $('<td></td>').append(checkbox);
 				rowElement.append(td);
 				//$('#readyForTransferIn:checkbox:checked')
 				//$('#' + id).is(":checked")
-			} else {
+            }
+            // If matrix status is 'Schedule' append here:
+            // If matrix status is 'Delegate' append here:
+            // If matrix status is 'Eliminate' append here:
+            else {
 				let checkbox = $('<input type="checkbox" class="readyToTransfer"/>').prop('checked', false).prop('disabled', false);
 				let td = $('<td></td>').append(checkbox);
 				rowElement.append(td);
 			}
-			rowElement.append(`<td>${notes}</td>`)
-			rowElement.append(
-				`<td><button class = "btn btn-danger deleteButton">Remove</button></td>`
-			);
+			// rowElement.append(`<td>${notes}</td>`)
+			// rowElement.append(
+			// 	`<td><button class = "btn btn-danger deleteButton">Remove</button></td>`
+			// );
 
-			$('table tbody').append(rowElement);
+			$('#doTable #doTbody').append(rowElement);
 		}
 	})
 }
