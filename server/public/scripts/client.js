e3625efe4eb6501df4a5d96200168a291840e251
Jake Schaffer <jakecr8guru@gmail.com>
2020-06-07 14:52:44 -0500

    added GET for variable of progress state to display on DOM

--- server/public/scripts/client.js
+++ server/public/scripts/client.js
@@ -1,19 +1,15 @@
 $(document).ready(readyNow);
 
 function readyNow() {
-    console.log('Testing testing 1 2 3...');
-    $('#sumbitTask').click(saveTasks);
-    $(".table").on("click", ".deleteButton", deleteTask);
-    $(".table").on("click", ".progressStatus", updateTaskProgressStatus);
-    refreshTasks();
+  console.log("Testing testing 1 2 3...");
+  setupClickListeners();
+  refreshTasks();
 }
 
-function addTaskClickHandler() {
-    // Inputs will need conditionals with levels that will automaticly make the value for matrix status 
-    const taskName = $('#taskNameIn').val();
-    const taskUrgenceLevel = $('#urgenceLevelIn').val();
-    const taskImportanceLevel = $('#importanceLevelIn').val();
-    const taskDetails = $('#taskDetailsIn').val();
+function setupClickListeners() {
+  $("#sumbitTask").click(saveTasks);
+  $(".table").on("click", ".deleteButton", deleteTask);
+  $(".table").on("click", ".progressStatus", updateTaskProgressStatus);
 }
 
 function updateTaskProgressStatus(event) {
@@ -29,114 +25,124 @@ function updateTaskProgressStatus(event) {
       // error, notify the user:,
       alert("Request failed. Try again later.");
     });
-    // I will need a whole server side HTTP request to now GET anything with complete:
-    // $(event.target).closest("tr").fadeOut().remove();
+  // I will need a whole server side HTTP request to now GET anything with complete:
+  // $(event.target).closest("tr").fadeOut().remove();
 }
 
 function saveTasks() {
-    console.log('In saveTasks function!');
-	if (validateInputs()) {
-		// checks if the inputs are infact invald, if so, return false.
-		return false;
-	}
-	// ajax call to server to get tasks
-	const taskName = $('#taskNameIn').val();
-	const taskUrgenceLevel = $('#urgenceLevelIn').val();
-	const taskImportanceLevel = $('#importanceLevelIn').val();
-	const taskDetails = $('#taskDetailsIn').val();
+  console.log("In saveTasks function!");
+  if (validateInputs()) {
+    // checks if the inputs are infact invald, if so, return false.
+    return false;
+  }
+  // ajax call to server to get tasks
+  const taskName = $("#taskNameIn").val();
+  const taskUrgenceLevel = $("#urgenceLevelIn").val();
+  const taskImportanceLevel = $("#importanceLevelIn").val();
+  const taskDetails = $("#taskDetailsIn").val();
 
-	$.ajax({
-		method: 'POST',
-		url: '/tasks',
-		data: {
-			task_name: taskName,
-			task_details: taskDetails,
-			matrix_status: 'Do',
-            urgence_level: taskUrgenceLevel,
-            importance_level: taskImportanceLevel,
-            progress_state: 'Inprogress',
-		}
-	}).then(function (response) {
-
-		// update the history
-		refreshTasks();
-		clearInputs();
-	}).catch(function (response) {
-		// only called when server returns 4xx 5xx
-		alert('something went wrong');
-	})
+  $.ajax({
+    method: "POST",
+    url: "/tasks",
+    data: {
+      task_name: taskName,
+      task_details: taskDetails,
+      matrix_status: "Do",
+      urgence_level: taskUrgenceLevel,
+      importance_level: taskImportanceLevel,
+      progress_state: "Inprogress",
+    },
+  })
+    .then(function (response) {
+      // update the history
+      refreshTasks();
+      clearInputs();
+    })
+    .catch(function (response) {
+      // only called when server returns 4xx 5xx
+      alert("something went wrong");
+    });
 }
 
 function validateInputs() {
-	if ($('#taskNameIn').val() === '' ||
-		$('#urgenceLevelIn').val() === '' ||
-		$('#importanceLevelIn').val() === '' ||
-		$('#taskDetailsIn').val() === ''){
-		alert('You must fill all input fields out!')
-		return true;
-	}
+  if (
+    $("#taskNameIn").val() === "" ||
+    $("#urgenceLevelIn").val() === "" ||
+    $("#importanceLevelIn").val() === ""
+  ) {
+    alert("You must fill all input fields out!");
+    return true;
+  }
 }
 
 function clearInputs() {
-	// function to clear the input fields
-	$('#taskNameIn').val('');
-	$('#urgenceLevelIn').val('');
-	$('#importanceLevelIn').val('');
-	$('#taskDetailsIn').val('');
+  // function to clear the input fields
+  $("#taskNameIn").val("");
+  $("#urgenceLevelIn").val("");
+  $("#importanceLevelIn").val("");
+  $("#taskDetailsIn").val("");
 }
 
 // Function GETs data and displays it
 function refreshTasks() {
-    console.log('In refreshTask function!');
-	$.ajax({
-		method: 'GET',
-		url: '/tasks',
-	}).then(function (response) {
-		$('#doTable #doTbody').empty();
-		for (const task of response) {
-            let rowElement = $('<tr class="table-success"></tr>');
-            // Will need to think more about how and where to use these
-			const name = task.task_name;
-			const details = task.task_details;
-			const matrixStatus = task.matrix_status;
-			const urgence = task.urgence_level;
-			const importance = task.importance_level;
-            const progressState = task.progress_state;
-            // What do I actually need to append in the table?
-            rowElement.data('task', task);
-            // Need to make this a variable that increments
-            rowElement.append(`<th scope="row">1</th>`);
-            //////////////////////////////////////////////
-			rowElement.append(`<td>${name}</td>`);
-			// rowElement.append(`<td>${details}</td>`);
-            // rowElement.append(`<td>${progressState}</td>`);
-            // If matrix status is 'Do' append here:
-            // I could use this checkbox for my progress state...
-			if (progressState === 'Complete') {
-				let checkbox = $('<input type="checkbox" aria-label="Checkbox for following text input"/>').prop('checked', true).prop('disabled', true);
-				let td = $('<td></td>').append(checkbox);
-				rowElement.append(td);
-				//$('#readyForTransferIn:checkbox:checked')
-				//$('#' + id).is(":checked")
-            }
-            // If matrix status is 'Schedule' append here:
-            // If matrix status is 'Delegate' append here:
-            // If matrix status is 'Eliminate' append here:
-            else {
-				let checkbox = $('<input type="checkbox" class="progressStatus" aria-label="Checkbox for following text input"/>').prop('checked', false).prop('disabled', false);
-				let td = $('<td></td>').append(checkbox);
-				rowElement.append(td);
-			}
-			// rowElement.append(`<td>${notes}</td>`)
-			// rowElement.append(
-			// 	`<td><button class = "btn btn-danger deleteButton">Remove</button></td>`
-            // );
-            rowElement.append(
-              `<td><button class = "btn btn-outline-danger deleteButton">X</button></td>`
-            );
-			$('#doTable #doTbody').append(rowElement);
-		}
-	})
+  console.log("In refreshTask function!");
+  $.ajax({
+    method: "GET",
+    url: "/tasks",
+  }).then(function (response) {
+    $("#doTable #doTbody").empty();
+    for (const task of response) {
+      let rowElement = $('<tr class="table-success"></tr>');
+      // Will need to think more about how and where to use these
+      const name = task.task_name;
+      const details = task.task_details;
+      const matrixStatus = task.matrix_status;
+      const urgence = task.urgence_level;
+      const importance = task.importance_level;
+      const progressState = task.progress_state;
+      // What do I actually need to append in the table?
+      rowElement.data("task", task);
+      // Need to make this a variable that increments
+      rowElement.append(`<th scope="row">1</th>`);
+      //////////////////////////////////////////////
+      rowElement.append(`<td>${name}</td>`);
+      // rowElement.append(`<td>${details}</td>`);
+      // rowElement.append(`<td>${progressState}</td>`);
+      // If matrix status is 'Do' append here:
+      // I could use this checkbox for my progress state...
+      if (progressState === "Complete") {
+        let checkbox = $(
+          '<input type="checkbox" aria-label="Checkbox for following text input"/>'
+        )
+          .prop("checked", true)
+          .prop("disabled", true);
+        let td = $("<td></td>").append(checkbox);
+        rowElement.append(td);
+        //$('#readyForTransferIn:checkbox:checked')
+        //$('#' + id).is(":checked")
+      }
+      // If matrix status is 'Schedule' append here:
+      // If matrix status is 'Delegate' append here:
+      // If matrix status is 'Eliminate' append here:
+      else {
+        let checkbox = $(
+          '<input type="checkbox" class="progressStatus" aria-label="Checkbox for following text input"/>'
+        )
+          .prop("checked", false)
+          .prop("disabled", false);
+        let td = $("<td></td>").append(checkbox);
+        rowElement.append(td);
+      }
+      // rowElement.append(`<td>${notes}</td>`)
+      // rowElement.append(
+      // 	`<td><button class = "btn btn-danger deleteButton">Remove</button></td>`
+      // );
+      rowElement.append(
+        `<td><button class = "btn btn-outline-danger deleteButton">X</button></td>`
+      );
+      $("#doTable #doTbody").append(rowElement);
+    }
+  });
 }
 
 function deleteTask(event) {
--- server/routes/task.router.js
+++ server/routes/task.router.js
@@ -1,91 +1,123 @@
-const pool = require('../modules/pool.js')
-const express = require('express');
+const pool = require("../modules/pool.js");
+const express = require("express");
 const taskRouter = express.Router();
 
-pool.on('connect', () => {
-	console.log('Postgresql connected');
+pool.on("connect", () => {
+  console.log("Postgresql connected");
 });
 
-pool.on('error', (error) => {
-	console.log('Error with postgres pool', error)
+pool.on("error", (error) => {
+  console.log("Error with postgres pool", error);
 });
 
 // GET
 
 // GETs all the tasks from database
-taskRouter.get('/', (req, res) => {
-    pool.query('SELECT * FROM "tasks" ORDER BY "task_name";')
-        .then((result) => {
-            console.log(`Here is everything in /tasks`);
-            res.status(200).send(result.rows);
-        })
-        .catch((err) => res.status(500).send(`Error Connecting to Database: ${err}`));
+taskRouter.get("/", (req, res) => {
+  pool
+    .query('SELECT * FROM "tasks" ORDER BY "task_name";')
+    .then((result) => {
+      console.log(`Here is everything in /tasks`);
+      res.status(200).send(result.rows);
+    })
+    .catch((err) =>
+      res.status(500).send(`Error Connecting to Database: ${err}`)
+    );
+});
+
+// I need a GET that only gets the progress_status of Inprogress
+taskRouter.get("/:progress", (req, res) => {
+  let task = req.body; // task with updated content
+  let progress = req.params.progress; // id of the task to update
+  console.log(task);
+  let queryText = 'SELECT * FROM "tasks" WHERE "progress_state" = $1 ORDER BY "task_name"';
+  pool
+    .query(queryText, [progress])
+    .then((result) => {
+      console.log(`Here are task that are in progress`);
+      res.status(200).send(result.rows);
+    })
+    .catch((err) =>
+      res.status(500).send(`Error Connecting to Database: ${err}`)
+    );
 });
 
 // POST
 
 // POSTs new task into database
-taskRouter.post('/', (req, res) => {
-	// get the task object
-	const newTask = req.body;
-	console.log(req.body);
-	// typecast the ready_to_transfer string into boolean. must be "==" to work!
-	// newTask.ready_to_transfer = newTask.ready_to_transfer;
-	// console.log(newTask.ready_to_transfer);
+taskRouter.post("/", (req, res) => {
+  // get the task object
+  const newTask = req.body;
+  console.log(req.body);
+  // typecast the ready_to_transfer string into boolean. must be "==" to work!
+  // newTask.ready_to_transfer = newTask.ready_to_transfer;
+  // console.log(newTask.ready_to_transfer);
 
-	const queryText = `INSERT INTO "tasks" (task_name, task_details, matrix_status, due_date, schedule_date, urgence_level, importance_level, progress_state) 
+  const queryText = `INSERT INTO "tasks" (task_name, task_details, matrix_status, due_date, schedule_date, urgence_level, importance_level, progress_state) 
         VALUES ($1, $2, $3, $4, $5, $6, $7, $8);`;
-	pool.query(queryText, [newTask.task_name, newTask.task_details, newTask.matrix_status, newTask.due_date, newTask.schedule_date, newTask.urgence_level, newTask.importance_level, newTask.progress_state])
-		.then((result) => {
-			res.status(201).send(`${newTask} was added to the database!`);
-		})
-		.catch((err) => {
-			console.log(`Error making query ${queryText}`, err);
-			res.sendStatus(500);
-		});
+  pool
+    .query(queryText, [
+      newTask.task_name,
+      newTask.task_details,
+      newTask.matrix_status,
+      newTask.due_date,
+      newTask.schedule_date,
+      newTask.urgence_level,
+      newTask.importance_level,
+      newTask.progress_state,
+    ])
+    .then((result) => {
+      res.status(201).send(`${newTask} was added to the database!`);
+    })
+    .catch((err) => {
+      console.log(`Error making query ${queryText}`, err);
+      res.sendStatus(500);
+    });
 });
 
 // PUT
 
 // Request must include a parameter indicating what task to update - the id
 // Request body must include the content to update - the status
-taskRouter.put('/:id',  (req, res) => {
-    let task = req.body; // koala with updated content
-    let id = req.params.id; // id of the koala to update
+taskRouter.put("/:id", (req, res) => {
+  let task = req.body; // koala with updated content
+  let id = req.params.id; // id of the koala to update
 
-    console.log(task);
-    let queryTextForUpdate = `UPDATE "tasks" SET "progress_state"='Complete' WHERE id = $1;`
-    pool.query(queryTextForUpdate, [id])
-        .then((result) => {
-            console.log(result.command);
-            res.status(200).send(`Updating tasks ${id} with , ${task}`);
-        })
-        .catch((error) => {
-            console.log(error);
-            res.sendStatus(500);
-        });
+  console.log(task);
+  let queryTextForUpdate = `UPDATE "tasks" SET "progress_state" = 'Complete' WHERE id = $1;`;
+  pool
+    .query(queryTextForUpdate, [id])
+    .then((result) => {
+      console.log(result.command);
+      res.status(200).send(`Updating tasks ${id} with , ${task}`);
+    })
+    .catch((error) => {
+      console.log(error);
+      res.sendStatus(500);
+    });
 });
 
 // DELETE
 
 // DELETEs a task from the DB
 // Request must include a parameter indicating what book to update - the id
-taskRouter.delete('/:id', (req, res) => {
-	//let testArray = [req.params]
-	let id = req.params.id; // id of the thing to delete
-	console.log('Delete route called with id of', id);
-	// let queryTextForSelect = `SELECT * FROM "books" WHERE "id" = $1;`
-	let queryTextForDelete = `DELETE FROM "tasks" WHERE "id" = $1;`
-	// TODO - REPLACE BELOW WITH YOUR CODE
-	pool.query(queryTextForDelete, [id])
-		.then((result) => {
-			console.log(result);
-			res.status(200).send(result.rows);
-		})
-		.catch((error) => {
-			console.log(error);
-			res.sendStatus(500);
-		});
+taskRouter.delete("/:id", (req, res) => {
+  //let testArray = [req.params]
+  let id = req.params.id; // id of the thing to delete
+  console.log("Delete route called with id of", id);
+  // let queryTextForSelect = `SELECT * FROM "books" WHERE "id" = $1;`
+  let queryTextForDelete = `DELETE FROM "tasks" WHERE "id" = $1;`;
+  // TODO - REPLACE BELOW WITH YOUR CODE
+  pool
+    .query(queryTextForDelete, [id])
+    .then((result) => {
+      console.log(result);
+      res.status(200).send(result.rows);
+    })
+    .catch((error) => {
+      console.log(error);
+      res.sendStatus(500);
+    });
 });
 
 module.exports = taskRouter;
