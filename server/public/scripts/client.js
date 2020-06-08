21905b6afa8f2e9efef2e389c4b49417bb329bbc
Jake Schaffer <jakecr8guru@gmail.com>
2020-06-07 22:35:54 -0500

    fixed bug with help from blaine

--- server/public/scripts/client.js
+++ server/public/scripts/client.js
@@ -3,7 +3,12 @@ $(document).ready(readyNow);
 function readyNow() {
 	console.log("Testing testing 1 2 3...");
 	setupClickListeners();
-	refreshTasks();
+	// refreshTasks();
+
+	refreshDoTasks();
+	refreshScheduleTasks();
+	refreshDelegateTasks();
+	refreshEliminateTasks();
 }
 
 // function listens for clicks
@@ -21,7 +26,11 @@ function updateTaskProgressStatus(event) {
 			url: `/tasks/${taskID}`,
 		})
 		.then((response) => {
-			refreshTasks();
+			// refreshTasks();
+			refreshDoTasks();
+			refreshScheduleTasks();
+			refreshDelegateTasks();
+			refreshEliminateTasks();
 		})
 		.catch((response) => {
 			// error, notify the user:,
@@ -40,22 +49,40 @@ function saveTasks() {
 	const taskUrgenceLevel = $("#urgenceLevelIn").val();
 	const taskImportanceLevel = $("#importanceLevelIn").val();
 	const taskDetails = $("#taskDetailsIn").val();
+	let taskMatrixStatus = '';
+
+	if (taskUrgenceLevel === "Urgent" && taskImportanceLevel === 'Important') {
+		taskMatrixStatus = 'Do';
+	} else if (taskUrgenceLevel === "Non-urgent" && taskImportanceLevel === 'Important') {
+		taskMatrixStatus = "Schedule";
+	} else if (taskUrgenceLevel === "Urgent" && taskImportanceLevel === 'Non-important') {
+		taskMatrixStatus = "Delegate";
+	} else if (taskUrgenceLevel === "Non-urgent" && taskImportanceLevel === 'Non-important') {
+		taskMatrixStatus = "Eliminate";
+	}
+	let matrixObj = {
+		task_name: taskName,
+		task_details: taskDetails,
+		matrix_status: taskMatrixStatus,
+		urgence_level: taskUrgenceLevel,
+		importance_level: taskImportanceLevel,
+		progress_state: "Inprogress", // Default
+	};
+
+	console.log(matrixObj);
 
 	$.ajax({
 			method: "POST",
 			url: "/tasks",
-			data: {
-				task_name: taskName,
-				task_details: taskDetails,
-				matrix_status: "Do",
-				urgence_level: taskUrgenceLevel,
-				importance_level: taskImportanceLevel,
-				progress_state: "Inprogress",
-			},
+			data: matrixObj,
 		})
 		.then(function (response) {
 			// update the history
-			refreshTasks();
+			// refreshTasks();
+			refreshDoTasks();
+			refreshScheduleTasks();
+			refreshDelegateTasks();
+			refreshEliminateTasks();
 			clearInputs();
 		})
 		.catch(function (response) {
@@ -91,19 +118,18 @@ function refreshTasks() {
 		method: "GET",
 		url: "/tasks/Inprogress",
 	}).then(function (response) {
-		$("#doTable #doTbody").empty();
-        let spot = 0;
-        let option = null;
+		// $("#doTable #doTbody").empty();
+		// $("#scheduleTable #scheduleTbody").empty();
+		// $("#delegateTable #delegateTbody").empty();
+		// $("#eliminateTable #eliminateTbody").empty();
+		let spot = 0;
+		let option = null;
 		for (const task of response) {
 			spot++;
 			let rowElement = $('<tr></tr>');
 			const name = task.task_name;
-			const details = task.task_details;
 			const matrixStatus = task.matrix_status;
-			//   const urgence = task.urgence_level;
-			//   const importance = task.importance_level;
 			const progressState = task.progress_state;
-			// What do I actually need to append in the table?
 			rowElement.data("task", task);
 			rowElement.append(`<th scope="row">${spot}</th>`);
 			rowElement.append(`<td>${name}</td>`);
@@ -128,34 +154,192 @@ function refreshTasks() {
 				`<td><button class = "btn btn-outline-danger deleteButton">X</button></td>`
 			);
 
-			if (matrixStatus == 'Do') {
-                console.log('Does it equal do');
-				option = 1;
-			} else if (matrixStatus == 'Schedule') {
-                console.log("Does it equal schedule");
-				option = 2;
-			} else if (matrixStatus == 'Delegate') {
-                console.log("Does it equal delegate");
-				option = 3;
-			} else if (matrixStatus == 'Eliminate') {
-                console.log("Does it equal eliminate");
-				option = 4;
-            }
-            
-            if (option === 1) {
-              console.log("Does it equal do");
-              $("#doTable #doTbody").append(rowElement);
-            } else if (option === 2) {
-              console.log("Does it equal schedule");
-              $("#scheduleTable #scheduleTbody").append(rowElement);
-            } else if (option === 3) {
-              console.log("Does it equal delegate");
-              $("#delegateTable #delegateTbody").append(rowElement);
-            } else if (option === 4) {
-              console.log("Does it equal eliminate");
-              $("#eliminateTable #eliminateTbody").append(rowElement);
-            }
-        }
+			// if (matrixStatus == 'Do') {
+			//     console.log('Does it equal do');
+			// 	option = 1;
+			// } else if (matrixStatus == 'Schedule') {
+			//     console.log("Does it equal schedule");
+			// 	option = 2;
+			// } else if (matrixStatus == 'Delegate') {
+			//     console.log("Does it equal delegate");
+			// 	option = 3;
+			// } else if (matrixStatus == 'Eliminate') {
+			//     console.log("Does it equal eliminate");
+			// 	option = 4;
+			// }
+
+			// if (option === 1) {
+			//   console.log("Does it equal do");
+			//   $("#doTable #doTbody").append(rowElement);
+			// } else if (option === 2) {
+			//   console.log("Does it equal schedule");
+			//   $("#scheduleTable #scheduleTbody").append(rowElement);
+			// } else if (option === 3) {
+			//   console.log("Does it equal delegate");
+			//   $("#delegateTable #delegateTbody").append(rowElement);
+			// } else if (option === 4) {
+			//   console.log("Does it equal eliminate");
+			//   $("#eliminateTable #eliminateTbody").append(rowElement);
+			// }
+		}
+	});
+}
+
+// function GETs data display on the ---- Do matrix
+function refreshDoTasks() {
+	console.log("In refreshDoTasks function!");
+	$.ajax({
+		method: 'GET',
+		url: '/tasks/status/Do',
+	}).then(function (response) {
+		$('#doTable #doTbody').empty();
+		let spot = 0;
+		for (let task of response) {
+			spot++;
+			let rowElement = $('<tr></tr>');
+			const name = task.task_name;
+			const progressState = task.progress_state;
+			rowElement.data('task', task);
+			rowElement.append(`<th scope="row">${spot}</th>`);
+			rowElement.append(`<td>${name}</td>`);
+			if (progressState === "Complete") {
+				let checkbox = $('<input type="checkbox" aria-label="Checkbox for following text input"/>').prop("checked", true).prop("disabled", true);
+				let td = $("<td></td>").append(checkbox);
+				rowElement.append(td);
+			} else {
+				let checkbox = $('<input type="checkbox" class="progressStatus" aria-label="Checkbox for following text input"/>').prop("checked", false).prop("disabled", false);
+				let td = $("<td></td>").append(checkbox);
+				rowElement.append(td);
+			}
+			rowElement.append(`<td><button class = "btn btn-outline-danger deleteButton">X</button></td>`);
+			$("#doTable #doTbody").append(rowElement);
+		}
+	});
+}
+
+// function GETs data display on the ---- Schedule matrix
+function refreshScheduleTasks() {
+	console.log("In refreshScheduleTasks function!");
+	$.ajax({
+		method: "GET",
+		url: "/tasks/status/Schedule",
+	}).then(function (response) {
+		$("#scheduleTable #scheduleTbody").empty();
+		let spot = 0;
+		for (const task of response) {
+			spot++;
+			let rowElement = $('<tr></tr>');
+			const name = task.task_name;
+			const progressState = task.progress_state;
+			rowElement.data("task", task);
+			rowElement.append(`<th scope="row">${spot}</th>`);
+			rowElement.append(`<td>${name}</td>`);
+			if (progressState === "Complete") {
+				let checkbox = $(
+						'<input type="checkbox" aria-label="Checkbox for following text input"/>'
+					)
+					.prop("checked", true)
+					.prop("disabled", true);
+				let td = $("<td></td>").append(checkbox);
+				rowElement.append(td);
+			} else {
+				let checkbox = $(
+						'<input type="checkbox" class="progressStatus" aria-label="Checkbox for following text input"/>'
+					)
+					.prop("checked", false)
+					.prop("disabled", false);
+				let td = $("<td></td>").append(checkbox);
+				rowElement.append(td);
+			}
+			rowElement.append(
+				`<td><button class = "btn btn-outline-danger deleteButton">X</button></td>`
+			);
+			$("#scheduleTable #scheduleTbody").append(rowElement);
+		}
+	});
+}
+
+// function GETs data display on the ---- Delegate matrix
+function refreshDelegateTasks() {
+	console.log("In refreshDelegateTasks function!");
+	$.ajax({
+		method: "GET",
+		url: "/tasks/status/Delegate",
+	}).then(function (response) {
+		$("#delegateTable #delegateTbody").empty();
+		let spot = 0;
+		for (const task of response) {
+			spot++;
+			let rowElement = $('<tr></tr>');
+			const name = task.task_name;
+			const progressState = task.progress_state;
+			rowElement.data("task", task);
+			rowElement.append(`<th scope="row">${spot}</th>`);
+			rowElement.append(`<td>${name}</td>`);
+			if (progressState === "Complete") {
+				let checkbox = $(
+						'<input type="checkbox" aria-label="Checkbox for following text input"/>'
+					)
+					.prop("checked", true)
+					.prop("disabled", true);
+				let td = $("<td></td>").append(checkbox);
+				rowElement.append(td);
+			} else {
+				let checkbox = $(
+						'<input type="checkbox" class="progressStatus" aria-label="Checkbox for following text input"/>'
+					)
+					.prop("checked", false)
+					.prop("disabled", false);
+				let td = $("<td></td>").append(checkbox);
+				rowElement.append(td);
+			}
+			rowElement.append(
+				`<td><button class = "btn btn-outline-danger deleteButton">X</button></td>`
+			);
+			$("#delegateTable #delegateTbody").append(rowElement);
+		}
+	});
+}
+
+// function GETs data display on the ---- Eliminate matrix
+function refreshEliminateTasks() {
+	console.log("In refreshEliminateTasks function!");
+	$.ajax({
+		method: "GET",
+		url: "/tasks/status/Eliminate",
+	}).then(function (response) {
+		$("#eliminateTable #eliminateTbody").empty();
+		let spot = 0;
+		for (const task of response) {
+			spot++;
+			let rowElement = $('<tr></tr>');
+			const name = task.task_name;
+			const progressState = task.progress_state;
+			rowElement.data("task", task);
+			rowElement.append(`<th scope="row">${spot}</th>`);
+			rowElement.append(`<td>${name}</td>`);
+			if (progressState === "Complete") {
+				let checkbox = $(
+						'<input type="checkbox" aria-label="Checkbox for following text input"/>'
+					)
+					.prop("checked", true)
+					.prop("disabled", true);
+				let td = $("<td></td>").append(checkbox);
+				rowElement.append(td);
+			} else {
+				let checkbox = $(
+						'<input type="checkbox" class="progressStatus" aria-label="Checkbox for following text input"/>'
+					)
+					.prop("checked", false)
+					.prop("disabled", false);
+				let td = $("<td></td>").append(checkbox);
+				rowElement.append(td);
+			}
+			rowElement.append(
+				`<td><button class = "btn btn-outline-danger deleteButton">X</button></td>`
+			);
+			$("#eliminateTable #eliminateTbody").append(rowElement);
+		}
 	});
 }
 
@@ -167,7 +351,11 @@ function deleteTask(event) {
 			url: `/tasks/${taskId}`,
 		})
 		.then(function (response) {
-			refreshTasks();
+            // refreshTasks();
+            refreshDoTasks();
+            refreshScheduleTasks();
+            refreshDelegateTasks();
+            refreshEliminateTasks();
 		})
 		.catch(function (error) {
 			alert("Error on delete", error);
--- server/routes/task.router.js
+++ server/routes/task.router.js
@@ -3,76 +3,99 @@ const express = require("express");
 const taskRouter = express.Router();
 
 pool.on("connect", () => {
-  console.log("Postgresql connected");
+	console.log("Postgresql connected");
 });
 
 pool.on("error", (error) => {
-  console.log("Error with postgres pool", error);
+	console.log("Error with postgres pool", error);
 });
 
 // GET
 
 // GETs all the tasks from database
 taskRouter.get("/", (req, res) => {
-  pool
-    .query('SELECT * FROM "tasks" ORDER BY "task_name";')
-    .then((result) => {
-      console.log(`Here is everything in /tasks`);
-      res.status(200).send(result.rows);
-    })
-    .catch((err) =>
-      res.status(500).send(`Error Connecting to Database: ${err}`)
-    );
+	pool
+		.query('SELECT * FROM "tasks" ORDER BY "task_name";')
+		.then((result) => {
+			console.log(`Here is everything in /tasks`);
+			res.status(200).send(result.rows);
+		})
+		.catch((err) =>
+			res.status(500).send(`Error Connecting to Database: ${err}`)
+		);
 });
 
 // I need a GET that only gets the progress_status of Inprogress
 taskRouter.get("/:progress", (req, res) => {
-  let task = req.body; // task with updated content
-  let progress = req.params.progress; // id of the task to update
-  console.log(task);
-  let queryText = 'SELECT * FROM "tasks" WHERE "progress_state" = $1 ORDER BY "task_name"';
-  pool
-    .query(queryText, [progress])
-    .then((result) => {
-      console.log(`Here are task that are in progress`);
-      res.status(200).send(result.rows);
-    })
-    .catch((err) =>
-      res.status(500).send(`Error Connecting to Database: ${err}`)
-    );
+	let task = req.body; // task with updated content
+	let progress = req.params.progress; // id of the task to update
+	console.log(task);
+	let queryText = 'SELECT * FROM "tasks" WHERE "progress_state" = $1 ORDER BY "task_name"';
+	pool
+		.query(queryText, [progress])
+		.then((result) => {
+			console.log(`Here are task that are in progress`);
+			res.status(200).send(result.rows);
+		})
+		.catch((err) =>
+			res.status(500).send(`Error Connecting to Database: ${err}`)
+		);
+});
+
+// I need a GET that only gets the matrix status
+taskRouter.get("/status/:matrix", (req, res) => {
+	let task = req.body; // task with updated content
+	let matrix = req.params.matrix; // matrix of the task to update
+	console.log(matrix);
+	console.log('Here is the matrix status body:', task);
+	let queryText =
+		'SELECT * FROM "tasks" WHERE "matrix_status" = $1 ORDER BY "task_name"';
+	pool
+		.query(queryText, [matrix])
+		.then((result) => {
+			console.log('Here are tasks that are in progress from the', matrix);
+			res.status(200).send(result.rows);
+		})
+		.catch((err) =>
+			res.status(500).send(`Error Connecting to Database: ${err}`)
+		);
 });
 
 // POST
 
 // POSTs new task into database
 taskRouter.post("/", (req, res) => {
-  // get the task object
-  const newTask = req.body;
-  console.log(req.body);
-  // typecast the ready_to_transfer string into boolean. must be "==" to work!
-  // newTask.ready_to_transfer = newTask.ready_to_transfer;
-  // console.log(newTask.ready_to_transfer);
+	// get the task object
+	const newTask = req.body;
+	console.log(req.body);
 
-  const queryText = `INSERT INTO "tasks" (task_name, task_details, matrix_status, due_date, schedule_date, urgence_level, importance_level, progress_state) 
-        VALUES ($1, $2, $3, $4, $5, $6, $7, $8);`;
-  pool
-    .query(queryText, [
-      newTask.task_name,
-      newTask.task_details,
-      newTask.matrix_status,
-      newTask.due_date,
-      newTask.schedule_date,
-      newTask.urgence_level,
-      newTask.importance_level,
-      newTask.progress_state,
-    ])
-    .then((result) => {
-      res.status(201).send(`${newTask} was added to the database!`);
-    })
-    .catch((err) => {
-      console.log(`Error making query ${queryText}`, err);
-      res.sendStatus(500);
-    });
+	if (newTask.matrix_status === '') {
+		res.sendStatus(400);
+		return;
+	}
+	// typecast the ready_to_transfer string into boolean. must be "==" to work!
+	// newTask.ready_to_transfer = newTask.ready_to_transfer;
+	// console.log(newTask.ready_to_transfer);
+
+	const queryText = 'INSERT INTO "tasks" (task_name, task_details, matrix_status, due_date, schedule_date, urgence_level, importance_level, progress_state) VALUES ($1, $2, $3, $4, $5, $6, $7, $8);';
+	pool.query(queryText, [
+			newTask.task_name,
+			newTask.task_details,
+			newTask.matrix_status,
+			newTask.due_date,
+			newTask.schedule_date,
+			newTask.urgence_level,
+			newTask.importance_level,
+			newTask.progress_state,
+		])
+		.then((result) => {
+			console.log('finished posting!');
+			res.status(201);
+		})
+		.catch((err) => {
+			console.log(err);
+			res.sendStatus(500);
+		});
 });
 
 // PUT
@@ -80,21 +103,21 @@ taskRouter.post("/", (req, res) => {
 // Request must include a parameter indicating what task to update - the id
 // Request body must include the content to update - the status
 taskRouter.put("/:id", (req, res) => {
-  let task = req.body; // koala with updated content
-  let id = req.params.id; // id of the koala to update
+	let task = req.body; // koala with updated content
+	let id = req.params.id; // id of the koala to update
 
-  console.log(task);
-  let queryTextForUpdate = `UPDATE "tasks" SET "progress_state" = 'Complete' WHERE id = $1;`;
-  pool
-    .query(queryTextForUpdate, [id])
-    .then((result) => {
-      console.log(result.command);
-      res.status(200).send(`Updating tasks ${id} with , ${task}`);
-    })
-    .catch((error) => {
-      console.log(error);
-      res.sendStatus(500);
-    });
+	console.log(task);
+	let queryTextForUpdate = `UPDATE "tasks" SET "progress_state" = 'Complete' WHERE id = $1;`;
+	pool
+		.query(queryTextForUpdate, [id])
+		.then((result) => {
+			console.log(result.command);
+			res.status(200).send(result);
+		})
+		.catch((error) => {
+			console.log(error);
+			res.sendStatus(500);
+		});
 });
 
 // DELETE
@@ -102,22 +125,22 @@ taskRouter.put("/:id", (req, res) => {
 // DELETEs a task from the DB
 // Request must include a parameter indicating what book to update - the id
 taskRouter.delete("/:id", (req, res) => {
-  //let testArray = [req.params]
-  let id = req.params.id; // id of the thing to delete
-  console.log("Delete route called with id of", id);
-  // let queryTextForSelect = `SELECT * FROM "books" WHERE "id" = $1;`
-  let queryTextForDelete = `DELETE FROM "tasks" WHERE "id" = $1;`;
-  // TODO - REPLACE BELOW WITH YOUR CODE
-  pool
-    .query(queryTextForDelete, [id])
-    .then((result) => {
-      console.log(result);
-      res.status(200).send(result.rows);
-    })
-    .catch((error) => {
-      console.log(error);
-      res.sendStatus(500);
-    });
+	//let testArray = [req.params]
+	let id = req.params.id; // id of the thing to delete
+	console.log("Delete route called with id of", id);
+	// let queryTextForSelect = `SELECT * FROM "books" WHERE "id" = $1;`
+	let queryTextForDelete = `DELETE FROM "tasks" WHERE "id" = $1;`;
+	// TODO - REPLACE BELOW WITH YOUR CODE
+	pool
+		.query(queryTextForDelete, [id])
+		.then((result) => {
+			console.log(result);
+			res.status(200).send(result.rows);
+		})
+		.catch((error) => {
+			console.log(error);
+			res.sendStatus(500);
+		});
 });
 
 module.exports = taskRouter;
